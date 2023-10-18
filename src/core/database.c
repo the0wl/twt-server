a2749a5dcadc4f0c9fc4852fc94e4e01a24fd43e
Kelvin Seibt <kelvin.seibt@gmail.com>
2023-10-17 21:44:37 -0300

    fix: Update database.c

--- src/core/database.c
+++ src/core/database.c
@@ -3,7 +3,7 @@
 #include <sqlite3.h>
 #include "dotenv.h"
 
-*char selectById(*char table, int id) {
+char** selectById(char *table, int id) {    
     sqlite3 *db;
     char *errMessage = 0;
     int rc;
@@ -18,35 +18,49 @@
     char sql[100];
     sprintf(sql, "SELECT * FROM %s WHERE id = %d;", table, id);
 
-    rc = sqlite3_exec(db, sql, callback, 0, &errMessage);
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot run query %s\n\nError: %s\n", sql, errMessage);
         sqlite3_free(errMessage);
+    } else {
+        // salvar dados sobre query em um arquivo
     }
 
+    // Array de strings para armazenar os registros
+    char **registros = NULL;
+    int num_registros = 0;
+
+    // Executar a consulta e percorrer os resultados
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        // Processar cada coluna do resultado
+        int col_count = sqlite3_column_count(stmt);
+        
+        char *registro = (char *)malloc(256);
+        
+        if (registro == NULL) {
+            fprintf(stderr, "Erro ao alocar memória para registro.");
+            return 1;
+        }
+
+        // Construir o registro como uma string
+        registro[0] = '\0'; // Garantir que a string esteja vazia antes de adicionar dados
+        
+        for (int i = 0; i < col_count; i++) {
+            strcat(registro, sqlite3_column_text(stmt, i));
+            strcat(registro, "\t"); // Adicionar um separador entre os valores
+        }
+
+        // Adicionar o registro ao array de strings
+        registros = (char **)realloc(registros, (num_registros + 1) * sizeof(char *));
+        registros[num_registros] = registro;
+        num_registros++;
+    }
+
+    // Finalizar a consulta e fechar o banco de dados
+    sqlite3_finalize(stmt);
     sqlite3_close(db);
-    printf("%d", sum);
+
+    return registros;
 }
-
-sqlite3 *db;
-  char *errMessage = 0;
-  int rc;
-
-  rc = sqlite3_open("./src/db/note.db", &db);
-  if (rc) {
-    fprintf(stderr, "Não deu: %s\n", sqlite3_errmsg(db));
-    return 1;
-  }
-
-  char sql[100];
-  sprintf(sql, "INSERT INTO sums (num1,num2,result) VALUES (%d, %d, %d);", num1, num2, sum);
-
-  rc = sqlite3_exec(db, sql, callback, 0, &errMessage);
-  if (rc != SQLITE_OK) {
-    fprintf(stderr, "SQL ruim: %s\n", errMessage);
-    sqlite3_free(errMessage);
-  }
-
-  sqlite3_close(db);
-  printf("%d", sum);
